clean code, fix rotation augmentation

equi/byol.py
@@ -228,7 +228,7 @@ class BYOL(nn.Module):
         self.to(device)
 
         # send a mock image tensor to instantiate singleton parameters
-        # self.forward(torch.randn(2, 4, image_size, image_size, device=device))
+        self.forward(torch.randint(0, 255, (2, 4, image_size, image_size), device=device))
 
     @singleton('target_encoder')
     def _get_target_encoder(self):
@@ -256,11 +256,6 @@ class BYOL(nn.Module):
         if return_embedding:
             return self.online_encoder(x, return_projection = return_projection)
         image_one, image_two = self.augment1(x), self.augment2(x)
-        # from matplotlib import pyplot as plt
-        # plt.imshow(image_one[0, :3, :, :].permute(1, 2, 0).cpu().numpy())
-        # plt.savefig('image_one.png')
-        # plt.imshow(image_two[0, :3, :, :].permute(1, 2, 0).cpu().numpy())
-        # plt.savefig('image_two.png')
         image_one = image_one.to(torch.float32) / 255.
         image_two = image_two.to(torch.float32) / 255.
 

equi/train.py
@@ -182,7 +182,6 @@ def update_env_kwargs(vv):
             del new_vv[v]
     return new_vv
 
-
 def vv_to_args(vv):
     class VArgs(object):
         def __init__(self, vv):
@@ -192,7 +191,6 @@ def vv_to_args(vv):
     args = VArgs(vv)
 
     return args
-
 
 def run_task(vv, log_dir=None, exp_name=None):
     updated_vv = copy.copy(DEFAULT_CONFIG)
@@ -225,7 +223,6 @@ def get_info_stats(infos):
         stat_dict[key + '_final'] = np.mean(stat_dict_all[key][:, -1])
     return stat_dict
 
-
 def evaluate(env, agent, video_dir, num_episodes, L, step, args, device):
     all_ep_rewards = []
 
@@ -245,7 +242,7 @@ def evaluate(env, agent, video_dir, num_episodes, L, step, args, device):
                                                 use_pretrained_0_train=False,
                                                 path_pretrained='/home/hnguyen/cloth_smoothing/equiRL/model/pretrained_observation_model.pt')
         # load_model(bc_model_finetune_pretrained, '/home/hnguyen/cloth_smoothing/equiRL/model/BC_0_finetune_pretrained.pt').to(device)
-        load_model(bc_model_finetune_pretrained, '/home/hnguyen/cloth_smoothing/equiRL/model/BC.pt').to(device)
+        # load_model(bc_model_finetune_pretrained, '/home/hnguyen/cloth_smoothing/equiRL/model/BC.pt').to(device)
 
         bc_rnn_model_0_finetune_pretrained = RNN_MIMO_MLP(output_dim=8,
                                                       hidden_dim=256,
@@ -255,16 +252,17 @@ def evaluate(env, agent, video_dir, num_episodes, L, step, args, device):
                                                       rnn_type="LSTM",
                                                       use_pretrained_0_train=False,
                                                       path_pretrained='/home/hnguyen/cloth_smoothing/equiRL/model/pretrained_observation_model.pt').to(device)
-        load_model(bc_rnn_model_0_finetune_pretrained, '/home/hnguyen/cloth_smoothing/equiRL/model/BC_RNN.pt').to(device)
+        # load_model(bc_rnn_model_0_finetune_pretrained, '/home/hnguyen/cloth_smoothing/equiRL/model/BC_RNN.pt').to(device)
+        # load_model(bc_rnn_model_0_finetune_pretrained, '/home/hnguyen/cloth_smoothing/equiRL/model/BC_RNN_0_finetune_pretrained.pt').to(device)
 
         bet_model = BeT_model(input_dim=514,
                           act_dim=8,
                           n_clusters=64,
                           k_means_fit_steps=500,
-                          use_pretrained_0_train=False,
-                          path_pretrained='/home/hnguyen/cloth_smoothing/equiRL/model/BC_0_finetune_pretrained.pt').to(device)
+                          use_pretrained_0_train=True,
+                          path_pretrained='/home/hnguyen/cloth_smoothing/equiRL/model/pretrained_observation_model.pt').to(device)
         load_model(bet_model, '/home/hnguyen/cloth_smoothing/equiRL/model/BeT.pt').to(device)
-
+        # load_model(bet_model, '/home/hnguyen/cloth_smoothing/equiRL/model/BeT_0_finetune_pretrained.pt').to(device)
         start_time = time.time()
         prefix = 'stochastic_' if sample_stochastically else ''
         infos = []
@@ -272,8 +270,8 @@ def evaluate(env, agent, video_dir, num_episodes, L, step, args, device):
         test_expert = False
         vinn = False
         bc = False
-        bc_rnn = True
-        bet = False
+        bc_rnn = False
+        bet = True
         if vinn:
             name = 'vinn'
         elif bc:
@@ -338,16 +336,12 @@ def evaluate(env, agent, video_dir, num_episodes, L, step, args, device):
                     #     else:
                     #         action = agent.select_action(obs,picker_state)
                     obs = obs.to(dtype=torch.float32, device=device) / 255.0
-                    numpy_obs = obs.cpu().detach().numpy()
                     if bet:
-                        if episode_step == 0:
-                            for _ in range(5):
-                                obs_rnn.append(numpy_obs)
-                                picker_state_rnn.append(picker_state)
-                        else:
-                            obs_rnn.append(numpy_obs)
-                            picker_state_rnn.append(picker_state)
+                        numpy_obs = obs.cpu().detach().numpy()
+                        obs_rnn.append(numpy_obs)
+                        picker_state_rnn.append(picker_state)
                         obs = torch.from_numpy(np.stack(obs_rnn)).to(device)
+                        obs = obs.permute(1, 0, 2, 3, 4)
                         picker_state = torch.from_numpy(np.stack(picker_state_rnn)).to(device)
                     else:
                         picker_state = torch.tensor(picker_state).to(device)
@@ -363,7 +357,7 @@ def evaluate(env, agent, video_dir, num_episodes, L, step, args, device):
                         action, rnn_hidden_state = bc_rnn_model_0_finetune_pretrained.forward(obs, picker_state, rnn_hidden_state)
                         action = action[0].squeeze(0).cpu().detach().numpy()
                     elif bet:
-                        action = bet_model(obs, picker_state)
+                        action = bet_model(obs, picker_state).squeeze(0)
                         action = action.cpu().detach().numpy()
                     obs, reward, done, info = env.step(action)
                     picker_state = utils.get_picker_state(env)
@@ -574,8 +568,8 @@ def main(args):
     agent = None
 
     # utils.create_demonstration(env, video_dir, args.num_demonstrations, img_size=128)
-    utils.create_play_data(env, video_dir, args.num_demonstrations, img_size=128)
-    exit()
+    # utils.create_play_data(env, video_dir, args.num_demonstrations, img_size=128)
+    # exit()
 
 
     # for i in all_expert_data_planner:

equi/utils.py
@@ -12,12 +12,12 @@ import time
 from skimage.util.shape import view_as_windows
 from collections import deque
 from scipy.ndimage import affine_transform
-from equi.default_config import DEFAULT_CONFIG
-from equi.segment_tree import SumSegmentTree, MinSegmentTree
+# from equi.default_config import DEFAULT_CONFIG
+# from equi.segment_tree import SumSegmentTree, MinSegmentTree
 from matplotlib import pyplot as plt
 from PIL import Image
 from scipy.spatial import ConvexHull
-from softgym.utils.visualization import save_numpy_as_gif, make_grid
+# from softgym.utils.visualization import save_numpy_as_gif, make_grid
 import cv2
 import pandas as pd
 
@@ -543,14 +543,13 @@ def perturb(current_image, next_image, dxy1, dxy2, set_theta_zero=False, set_tra
     transform = get_image_transform(theta, trans, pivot)
     transform_params = theta, trans, pivot
 
-    rot = np.array([[np.cos(theta), -np.sin(theta)], 
-                    [np.sin(theta), np.cos(theta)]])
+    rot = np.array([[np.cos(theta), np.sin(theta)], 
+                    [-np.sin(theta), np.cos(theta)]])
     rotated_dxy1 = rot.dot(dxy1)
     rotated_dxy1 = np.clip(rotated_dxy1, -1, 1)
     
     rotated_dxy2 = rot.dot(dxy2)
     rotated_dxy2 = np.clip(rotated_dxy2, -1, 1)
-    # import ipdb; ipdb.set_trace()
     # Apply rigid transform to image and pixel labels.
     if current_image.shape[0] == 1:
         current_image = affine_transform(current_image[0], np.linalg.inv(transform), mode='nearest', order=1).reshape(current_image.shape)
@@ -567,7 +566,7 @@ def perturb(current_image, next_image, dxy1, dxy2, set_theta_zero=False, set_tra
 
 
 def get_random_image_transform_params(image_size):
-    theta = np.random.random() * 2*np.pi
+    theta = np.random.uniform(-1, 1) * np.pi / 6
     trans = np.random.randint(0, image_size[0]//10, 2) - image_size[0]//20
     pivot = (image_size[1] / 2, image_size[0] / 2)
     return theta, trans, pivot